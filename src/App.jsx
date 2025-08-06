fe59bd914417a1055599f12870336920e4f6abb9
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-08-06 11:17:12 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css"; 
+import {RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+   }
+
+])
+
+const App = () => { 
+  return <RouterProvider router={router} />
+}
+
+export default App
