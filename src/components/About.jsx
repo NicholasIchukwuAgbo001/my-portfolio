cdb66683a3ac7a7ff599722127d43fadd833cdd5
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-08-07 15:06:34 +0100

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,16 @@
 const About = () => {
   return (
-    <div>
-      about
-    </div>
-  )
-}
+    <section className="py-16 bg-slate-950 text-white">
+      <div className="max-w-4xl mx-auto px-4 text-center">
+        <p className="text-stone-300 uppercase tracking-wide text-sm mb-2">
+          Get to know me
+        </p>
+        <h2 className="text-3xl sm:text-5xl font-bold">
+          About <span className="text-blue-600">Me</span>
+        </h2>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
