33ea6ca23f37eefe4ec5910cceb456e656043608
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-08-07 14:56:48 +0100

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,26 @@
 import HeroSection from "../components/HeroSection";
 import Skills from "../components/Skills";
 import ProjectLists from "../components/ProjectLists";
+import About from "../components/About";
 
 const Home = () => {
   return (
     <div className="px-2 md:px-4">
+
       <section id="home">
         <HeroSection />
       </section>
+
       <section id="skills">
         <Skills />
       </section>
+
       <section id="work">
         <ProjectLists />
+      </section>
+
+      <section id="about">
+        <About />
       </section>
     </div>
   );
