46a25ea1fa99a17c4ecb4db83ea1eb5340a5c36e
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-08-07 10:28:28 +0100

    commit

--- src/components/OtherSkills.jsx
+++ src/components/OtherSkills.jsx
@@ -18,7 +18,7 @@ const OtherSkills = () => {
         {navLists.map((list, index) => (
           <li key={index} className="font-serif text-sm">
             <span
-              className="text-stone-300 border border-blue-700 px-4 py-2 rounded-full"
+              className="text-stone-300 border border-blue-700 px-4 py-2 rounded-full shadow-[0_0_10px_2px_rgba(37,99,235,0.3)] hover:shadow-[0_0_15px_3px_rgba(37,99,235,0.5)] hover:scale-105 hover:translate-x-1 transition-transform duration-300 ease-in-out"
             >
               {list.label}
             </span>
