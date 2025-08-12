a8543b6717ec10947b90e57cd27d0def0bc59a25
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-08-12 18:22:25 +0100

    commit

--- src/components/DirectMessage.jsx
+++ src/components/DirectMessage.jsx
@@ -1,28 +1,58 @@
+import { useState } from "react";
 import { FaEnvelope } from "react-icons/fa";
 
 const DirectMessage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData({ name: "", email: "", message: "" });
+  };
+
   return (
-    <div className="border-2 text-white p-6 rounded-xl shadow-lg  bg-gradient-to-r from-slate-700 via-slate-800 to-slate-950">
+    <div className="border-2 text-white p-6 rounded-xl shadow-lg bg-gradient-to-r from-slate-700 via-slate-800 to-slate-950">
       <h3 className="text-2xl font-bold mb-4">Send me a message</h3>
-      <form className="space-y-4">
+      <form className="space-y-4" onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           <input
+            name="name"
             type="text"
             placeholder="Your Name"
             required
+            value={formData.name}
+            onChange={handleChange}
             className="w-full p-3 text-slate-950 rounded-md border focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <input
+            name="email"
             type="email"
             placeholder="Email Address"
             required
+            value={formData.email}
+            onChange={handleChange}
             className="w-full p-3 text-slate-950 rounded-md border focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
         <textarea
+          name="message"
           placeholder="Your Message"
           rows="4"
           required
+          value={formData.message}
+          onChange={handleChange}
           className="w-full text-slate-950 p-3 rounded-md border focus:outline-none focus:ring-2 focus:ring-blue-500"
         ></textarea>
         <button
--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -4,14 +4,13 @@ const Mission = () => {
   return (
     <div className="p-6 text-left flex flex-col justify-center">
       <div className="bg-slate-900 p-4 rounded-lg shadow-md">
-      <h3 className="text-xl font-semibold mb-4 text-blue-500">My Mission</h3>
-      <p className="text-stone-300 leading-relaxed">
-        I believe technology should be a bridge that connect people and solves real-world problems. My passion is to create innovative solutions that make a positive impact on society and also crafting digital experiences that are not just functional but delightful and accessible to everyone.
-
-        <p className="py-5">
-            When i'm not coding, you can find me reading books, learning new things, or exploring the outdoors.
+        <h3 className="text-xl font-semibold mb-4 text-blue-500">My Mission</h3>
+        <p className="text-stone-300 leading-relaxed">
+          I believe technology should be a bridge that connect people and solves real-world problems. My passion is to create innovative solutions that make a positive impact on society and also crafting digital experiences that are not just functional but delightful and accessible to everyone.
         </p>
-      </p>
+        <p className="py-5">
+          When i'm not coding, you can find me reading books, learning new things, or exploring the outdoors.
+        </p>
       </div>
       <LoveBuilding />
     </div>
